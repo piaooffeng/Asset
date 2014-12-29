4798eeee59b1a7b0eec14e83e47a42f004f6ad85
DongYuan <daniel_y_mail@163.com>
2014-12-29 17:06:45 +0800

    测试推送

--- AssetApp/FirstViewController.m
+++ AssetApp/FirstViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "FirstViewController.h"
+#import <AFNetworking.h>
+
 
 @interface FirstViewController ()
 
@@ -17,11 +19,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+//    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+//    manager.responseSerializer = [[AFHTTPResponseSerializer alloc] init];
+//    [manager.requestSerializer setValue:@"application/soap+xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
+//    [manager.requestSerializer setQueryStringSerializationWithBlock:^NSString *(NSURLRequest *request, NSDictionary *parameters, NSError *__autoreleasing *error) {
+//        //return soapMessage;
+//        return @"";
+//    }];
+//    [manager POST:@"http://www.webxml.com.cn/WebServices/WeatherWebService.asmx" parameters:@"" success:^(AFHTTPRequestOperation *operation, id responseObject) {
+//        NSString *response = [[NSString alloc] initWithData:(NSData *)responseObject encoding:NSUTF8StringEncoding];
+//        NSLog(@"%@, %@", operation, response);
+//    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+//        NSString *response = [[NSString alloc] initWithData:(NSData *)[operation responseObject] encoding:NSUTF8StringEncoding];
+//        NSLog(@"%@, %@", operation, error);
+//    }];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
+    
+    
 }
 
 @end
